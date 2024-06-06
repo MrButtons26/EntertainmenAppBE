c0c070d75157f291128dd6a853b629ff9f45359b
MrButtons26 <princejackson400@gmail.com>
2024-06-06 15:53:34 +0530

    env variable

--- controller/languageController.js
+++ controller/languageController.js
@@ -1,21 +1,17 @@
-const axios=require(`axios`)
+const axios = require(`axios`);
 
-exports.getLanguages=async(req,res)=>{
-    try{
-const response = await axios.get(
-          `https://api.themoviedb.org/3/configuration/languages`,
-          {
-            headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNDM0OGU0YTI1NTBmYTJkZmI2YzhlZTBkMzIxYTJjNSIsInN1YiI6IjY2NGYzMDAyYjk0ZDRhY2RjZDRmODA5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2wCu4dTWX02meDPToYUxmESipbJ0luEbo_BZwFBiD6I",
-            },
-          }
-        );
-        res.status(response.status).json([
-     ...response.data
-])
-    }
-    catch(e){
-       console.log(e)
-    } 
-}
+exports.getLanguages = async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/configuration/languages`,
+      {
+        headers: {
+          Authorization: process.env.Authorization,
+        },
+      }
+    );
+    res.status(response.status).json([...response.data]);
+  } catch (e) {
+    console.log(e);
+  }
+};
--- controller/searchController.js
+++ controller/searchController.js
@@ -1,22 +1,20 @@
-const axios=require(`axios`)
+const axios = require(`axios`);
 
-
-exports.getSearch=async(req,res)=>{
-    const {name}=req.query
-    try{
-   const searchResults = await axios.get(
-        `https://api.themoviedb.org/3/search/multi?query=${name.split(` `)
-          .join(`%`)}&include_adult=false&language=en-US&page=1`,
-        {
-          headers: {
-            Authorization:
-              process.env.Authorization,
-          },
-        }
-      )
-      res.status(searchResults.status).json({...searchResults.data})
-    }
-    catch(e){
-     console.log(e)
-    }
-}
+exports.getSearch = async (req, res) => {
+  const { name } = req.query;
+  try {
+    const searchResults = await axios.get(
+      `https://api.themoviedb.org/3/search/multi?query=${name
+        .split(` `)
+        .join(`%`)}&include_adult=false&language=en-US&page=1`,
+      {
+        headers: {
+          Authorization: process.env.Authorization,
+        },
+      }
+    );
+    res.status(searchResults.status).json({ ...searchResults.data });
+  } catch (e) {
+    console.log(e);
+  }
+};
