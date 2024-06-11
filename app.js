04dc7be4c6f0288a1ccc551512e52b9b02ab5f86
MrButtons26 <princejackson400@gmail.com>
2024-06-11 18:34:31 +0530

    auth

--- app.js
+++ app.js
@@ -20,18 +20,23 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get(`/`, (req, res) => {
-  res.send('hello')
-})
+
 
 //routes for all the resources 
-app.use(`/language`, languageRouter);
 app.use(`/movies`, moviesRouter);
+app.use(`/language`, languageRouter);
 app.use(`/tvshows`, tvRouter);
 app.use(`/search`, searchRouter);
 app.use(`/user`, userRouter);
 app.use(`/bookmarks`, bookMarksRouter);
 app.use('/recommendations', recommendRouter)
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: `failed`,
+    message: `This Url does not belong to the server`
+  })
+})
+
 
 //opening port for the machine to listen to requests
 app.listen(process.env.PORT, () => {
